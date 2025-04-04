8e055cd096ce755556a37a705c712b4b2531440d
jaamarks <32715488+jaamarks@users.noreply.github.com>
2025-04-04 16:55:48 -0500

    feat: add runtype param to optionally lint configs
    Added a new `RUNTYPE` parameter to control whether the linter
    runs on a Nextflow script (`script`) or a Nextflow configuration
    file (`config`).
    
    - Updated the Dockerfile to include a new `RUNTYPE` environment
      variable, defaulting to `script`.
    - Modified `scripts/check.sh` to accept both `RULESET` and
      `RUNTYPE`, determining the target for linting.
    - Added validation to ensure `RUNTYPE` is either `script` or
      `config`.
    
    This change enhances flexibility, allowing users to lint both
    workflow scripts and configuration files.

--- Dockerfile
+++ Dockerfile
@@ -20,4 +20,5 @@ RUN wget https://repo1.maven.org/maven2/org/codenarc/CodeNarc/3.3.0/CodeNarc-3.3
     wget https://repo1.maven.org/maven2/org/slf4j/slf4j-simple/1.7.36/slf4j-simple-1.7.36.jar
 
 ENV ruleset "healthomics"
-ENTRYPOINT bash check.sh ${ruleset}
+ENV runtype "script"
+ENTRYPOINT bash check.sh ${ruleset} ${runtype}
--- scripts/check.sh
+++ scripts/check.sh
@@ -1,12 +1,26 @@
 #! /bin/bash
 
 RULESET="$1"
-echo  "Checking with ruleset: ${RULESET:=healthomics}"
+RUNTYPE="$2"
+echo "Checking with ruleset: ${RULESET:=healthomics}"
+echo "Running on type: ${RUNTYPE:=script}"
 
-java  -Dorg.slf4j.simpleLogger.defaultLogLevel=error \
+# Check if RUNTYPE is set to "script" or "config"
+if [ "$RUNTYPE" == "script" ]; then
+  INCLUDES="**/*.nf"
+elif [ "$RUNTYPE" == "config" ]; then
+  INCLUDES="**/*.config"
+else
+  # If RUN_TYPE is not "script" or "config", exit and show an error message
+  echo "Error: You need to specify either 'script' or 'config' as the argument."
+  exit 1
+fi
+
+# Run the Java command with the appropriate INCLUDES
+java -Dorg.slf4j.simpleLogger.defaultLogLevel=error \
   -classpath linter-rules.jar:CodeNarc-3.3.0-all.jar:slf4j-api-1.7.36.jar:slf4j-simple-1.7.36.jar \
   org.codenarc.CodeNarc \
   -report=text:stdout \
   -rulesetfiles=rulesets/"${RULESET}".xml \
   -basedir=/data \
-  -includes=**/*.nf
+  -includes="${INCLUDES}"
