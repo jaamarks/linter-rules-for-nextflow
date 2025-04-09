bab0ee542ab327a2b3796f536cafc1e1ab8cb251
jaamarks <32715488+jaamarks@users.noreply.github.com>
2025-04-09 13:01:57 -0500

    feat(linter): support block and assignment config syntax
    Nextflow configuration files can define settings using either block
    syntax or assignment syntax. This update extends the ConfigExecutorRule
    to support both styles for specifying the executor.
    
    Examples of supported syntax:
    ```
    process {
      executor = 'slurm'
    }
    ```
    
    and `process.executor = 'slurm'`
    This improves rule coverage and flexibility when parsing config files.

--- linter-rules/src/main/groovy/software/amazon/nextflow/rules/config/ConfigExecutorRule.groovy
+++ linter-rules/src/main/groovy/software/amazon/nextflow/rules/config/ConfigExecutorRule.groovy
@@ -1,17 +1,21 @@
-/*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-
 package software.amazon.nextflow.rules.config
+
+/*
+ * Check the executor specified in nextflow.config to ensure that a
+ * cloud-compatible executor is used instead of an HPC-based one.
+ *
+ * @author Jesse Marks
+ */
 
 import org.codehaus.groovy.ast.expr.BinaryExpression
 import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.PropertyExpression
 import org.codehaus.groovy.ast.expr.VariableExpression
 import org.codenarc.rule.AbstractAstVisitor
 import org.codenarc.rule.AbstractAstVisitorRule
 
 class ConfigExecutorRule extends AbstractAstVisitorRule {
+
     String name = 'ConfigExecutorRule'
     int priority = 1
     Class astVisitorClass = ConfigExecutorAstVisitor
@@ -27,20 +31,42 @@ class ConfigExecutorAstVisitor extends AbstractAstVisitor {
 
     @Override
     void visitBinaryExpression(BinaryExpression expression) {
+
         if (expression.leftExpression instanceof VariableExpression) {
-            def varExpression = expression.leftExpression as VariableExpression
-            if (varExpression.text == 'executor') {
-                if (expression.rightExpression instanceof ConstantExpression) {
-                    def constExpression = expression.rightExpression as ConstantExpression
-                    if ( HPC_EXECUTORS.contains(constExpression.value)){
-                        addViolation(constExpression, "Expected a cloud-based executor such as 'awsbatch', found '${constExpression.value}'.")
-                    }
-                } else {
-                    addViolation(expression.rightExpression, "Expected a string literal for executor such as 'awsbatch', found ${expression.rightExpression.text}")
+            checkVariableExpression(expression)
+        } else if (expression.leftExpression instanceof PropertyExpression) {
+            checkPropertyExpression(expression)
+        }
+        super.visitBinaryExpression(expression)
+    }
+
+    private checkVariableExpression(final BinaryExpression expression) {
+
+        def varExpression = expression.leftExpression as VariableExpression
+        if (varExpression.text == 'executor') {
+            if (expression.rightExpression instanceof ConstantExpression) {
+                def constExpression = expression.rightExpression as ConstantExpression
+                if ( HPC_EXECUTORS.contains(constExpression.value)){
+                    addViolation(constExpression, "Expected a cloud-based executor such as 'awsbatch', found '${constExpression.value}'.")
                 }
+            } else {
+                addViolation(expression.rightExpression, "Expected a string literal for executor such as 'awsbatch', found ${expression.rightExpression.text}")
             }
         }
+    }
 
-        super.visitBinaryExpression(expression)
+    private checkPropertyExpression(final BinaryExpression expression) {
+
+        def propExpression = expression.leftExpression as PropertyExpression
+        if (propExpression.text == 'process.executor') {
+            if (expression.rightExpression instanceof ConstantExpression) {
+                def constExpression = expression.rightExpression as ConstantExpression
+                if ( HPC_EXECUTORS.contains(constExpression.value) ) {
+                    addViolation(constExpression, "Expected a cloud-based executor such as 'awsbatch', found '${constExpression.value}'.")
+                }
+            } else {
+                addViolation(expression.rightExpression, "Expected a string literal for executor such as 'awsbatch', found ${expression.rightExpression.text}")
+            }
+        }
     }
 }
